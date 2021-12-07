9c3dca3d68029ee861756eb3e2709834c582769b
elahe412 <67527897+elahe412@users.noreply.github.com>
2021-12-07 19:37:16 +0330

    Update urls.py

--- apps/hi/urls.py
+++ apps/hi/urls.py
@@ -5,6 +5,7 @@ from apps.hi.views import say_hello, say_my_name, readiness, liveness
 urlpatterns = [
     path('say_hello/', say_hello),
     path('say_my_name/', say_my_name),
+    path('gender_recognition/', say_my_name),
     path('readiness/', readiness),
 #     path('healthz/', liveness),
 ]
