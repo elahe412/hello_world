bcde983ea6f98c30f26303d8dbb7d54d508236f1
elahe412 <67527897+elahe412@users.noreply.github.com>
2021-12-07 19:35:31 +0330

    Update urls.py

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core', include('apps.hi.urls')),
+    path('', include('apps.hi.urls')),
 ]
